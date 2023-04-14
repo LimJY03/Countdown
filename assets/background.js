be16ba9f16d94e7a7bf9abdd3f935f3fbc88a7f9
DesigneR- <limjunyi03@gmail.com>
2023-04-14 11:15:56 +0800

    Update Data Structure

--- assets/background.js
+++ assets/background.js
@@ -1,44 +1,54 @@
-let i = 0;
-const minute = 10;
+// Set Change Interval
+const MINUTE = 10;
 
-const imageUrls = [
-    './images/cover-image-2.jpg',
-    './images/cover-image-3.jpg',
-    './images/cover-image-4.jpg',
-    './images/cover-image-5.jpg',
-    './images/cover-image-1.jpg',
-];
-const backgroundStyleArr = [
-    'linear-gradient(135deg, rgba(27, 11, 21, 0.8), rgba(3, 14, 51, 0.8))',
-    'linear-gradient(225deg, rgba(27, 21, 11, 0.8), rgba(6, 3, 51, 0.8))',
-    'linear-gradient(315deg, rgba(27, 11, 11, 0.8), rgba(3, 20, 51, 0.8))',
-    'linear-gradient(45deg, rgba(49, 18, 18, 0.8), rgba(2, 31, 7, 0.8))',
-    'linear-gradient(45deg, rgba(20, 11, 27, 0.8), rgba(51, 3, 48, 0.8))',
-];
-const popColorArr = [
-    'rgb(250, 202, 222)',
-    'rgb(250, 221, 202)',
-    'rgb(202, 211, 250)',
-    'rgb(211, 230, 204)',
-    'rgb(232, 202, 250)',
-];
-const popColorSubtitleArr = [
-    'rgb(167, 142, 161)',
-    'rgb(167, 152, 142)',
-    'rgb(142, 154, 167)',
-    'rgb(151, 158, 151)',
-    'rgb(158, 142, 167)',
+// Define Theme Data
+const THEME = [
+    { 
+        imageUrl: './images/cover-image-1.jpg', 
+        bgStyle: 'linear-gradient(45deg, rgba(20, 11, 27, 0.8), rgba(51, 3, 48, 0.8))', 
+        popColor: 'rgb(232, 202, 250)', 
+        popColorSecondary: 'rgb(158, 142, 167)' 
+    },
+    { 
+        imageUrl: './images/cover-image-2.jpg', 
+        bgStyle: 'linear-gradient(135deg, rgba(27, 11, 21, 0.8), rgba(3, 14, 51, 0.8))', 
+        popColor: 'rgb(250, 202, 222)', 
+        popColorSecondary: 'rgb(167, 142, 161)' 
+    },
+    { 
+        imageUrl: './images/cover-image-3.jpg', 
+        bgStyle: 'linear-gradient(225deg, rgba(27, 21, 11, 0.8), rgba(6, 3, 51, 0.8))', 
+        popColor: 'rgb(250, 221, 202)', 
+        popColorSecondary: 'rgb(167, 152, 142)' 
+    },
+    { 
+        imageUrl: './images/cover-image-4.jpg', 
+        bgStyle: 'linear-gradient(315deg, rgba(27, 11, 11, 0.8), rgba(3, 20, 51, 0.8))', 
+        popColor: 'rgb(202, 211, 250)', 
+        popColorSecondary: 'rgb(142, 154, 167)' 
+    },
+    { 
+        imageUrl: './images/cover-image-5.jpg', 
+        bgStyle: 'linear-gradient(45deg, rgba(49, 18, 18, 0.8), rgba(2, 31, 7, 0.8))', 
+        popColor: 'rgb(211, 230, 204)', 
+        popColorSecondary: 'rgb(151, 158, 151)' 
+    },
 ];
 
-imageUrls.forEach((imgUrl) => new Image().src = imgUrl);
+// Image Caching
+THEME.forEach((elem) => new Image().src = elem.imageUrl);
+
+// Start Iterating
+let i = 1;
 
 window.setInterval(() => {
     
-    if (i == backgroundStyleArr.length) i = 0;
+    if (i == THEME.length) i = 0;
 
-    document.body.style.backgroundImage = `${backgroundStyleArr[i]}, url(${imageUrls[i]})`;
-    document.documentElement.style.setProperty('--color-pop', popColorArr[i]);
-    document.documentElement.style.setProperty('--color-pop-subtitle', popColorArr[i]);
+    document.body.style.backgroundImage = `${THEME[i].bgStyle}, url(${THEME[i].imageUrl})`;
+    document.documentElement.style.setProperty('--color-pop', THEME[i].popColor);
+    document.documentElement.style.setProperty('--color-pop-subtitle', THEME[i].popColorSecondary);
+
     i++;
 
-}, minute * 60 * 1_000);
+}, MINUTE * 60 * 1_000);
