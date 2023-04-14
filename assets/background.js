6616b0331b57af988ea4fd816992a8464e1dc655
DesigneR- <limjunyi03@gmail.com>
2023-04-14 11:17:23 +0800

    Update Naming

--- assets/background.js
+++ assets/background.js
@@ -5,33 +5,33 @@ const MINUTE = 10;
 const THEME = [
     { 
         imageUrl: './images/cover-image-1.jpg', 
-        bgStyle: 'linear-gradient(45deg, rgba(20, 11, 27, 0.8), rgba(51, 3, 48, 0.8))', 
-        popColor: 'rgb(232, 202, 250)', 
-        popColorSecondary: 'rgb(158, 142, 167)' 
+        imageOverlay: 'linear-gradient(45deg, rgba(20, 11, 27, 0.8), rgba(51, 3, 48, 0.8))', 
+        colorPrimary: 'rgb(232, 202, 250)', 
+        colorSecondary: 'rgb(158, 142, 167)' 
     },
     { 
         imageUrl: './images/cover-image-2.jpg', 
-        bgStyle: 'linear-gradient(135deg, rgba(27, 11, 21, 0.8), rgba(3, 14, 51, 0.8))', 
-        popColor: 'rgb(250, 202, 222)', 
-        popColorSecondary: 'rgb(167, 142, 161)' 
+        imageOverlay: 'linear-gradient(135deg, rgba(27, 11, 21, 0.8), rgba(3, 14, 51, 0.8))', 
+        colorPrimary: 'rgb(250, 202, 222)', 
+        colorSecondary: 'rgb(167, 142, 161)' 
     },
     { 
         imageUrl: './images/cover-image-3.jpg', 
-        bgStyle: 'linear-gradient(225deg, rgba(27, 21, 11, 0.8), rgba(6, 3, 51, 0.8))', 
-        popColor: 'rgb(250, 221, 202)', 
-        popColorSecondary: 'rgb(167, 152, 142)' 
+        imageOverlay: 'linear-gradient(225deg, rgba(27, 21, 11, 0.8), rgba(6, 3, 51, 0.8))', 
+        colorPrimary: 'rgb(250, 221, 202)', 
+        colorSecondary: 'rgb(167, 152, 142)' 
     },
     { 
         imageUrl: './images/cover-image-4.jpg', 
-        bgStyle: 'linear-gradient(315deg, rgba(27, 11, 11, 0.8), rgba(3, 20, 51, 0.8))', 
-        popColor: 'rgb(202, 211, 250)', 
-        popColorSecondary: 'rgb(142, 154, 167)' 
+        imageOverlay: 'linear-gradient(315deg, rgba(27, 11, 11, 0.8), rgba(3, 20, 51, 0.8))', 
+        colorPrimary: 'rgb(202, 211, 250)', 
+        colorSecondary: 'rgb(142, 154, 167)' 
     },
     { 
         imageUrl: './images/cover-image-5.jpg', 
-        bgStyle: 'linear-gradient(45deg, rgba(49, 18, 18, 0.8), rgba(2, 31, 7, 0.8))', 
-        popColor: 'rgb(211, 230, 204)', 
-        popColorSecondary: 'rgb(151, 158, 151)' 
+        imageOverlay: 'linear-gradient(45deg, rgba(49, 18, 18, 0.8), rgba(2, 31, 7, 0.8))', 
+        colorPrimary: 'rgb(211, 230, 204)', 
+        colorSecondary: 'rgb(151, 158, 151)' 
     },
 ];
 
@@ -45,9 +45,9 @@ window.setInterval(() => {
     
     if (i == THEME.length) i = 0;
 
-    document.body.style.backgroundImage = `${THEME[i].bgStyle}, url(${THEME[i].imageUrl})`;
-    document.documentElement.style.setProperty('--color-pop', THEME[i].popColor);
-    document.documentElement.style.setProperty('--color-pop-subtitle', THEME[i].popColorSecondary);
+    document.body.style.backgroundImage = `${THEME[i].imageOverlay}, url(${THEME[i].imageUrl})`;
+    document.documentElement.style.setProperty('--color-pop', THEME[i].colorPrimary);
+    document.documentElement.style.setProperty('--color-pop-subtitle', THEME[i].colorSecondary);
 
     i++;
 
